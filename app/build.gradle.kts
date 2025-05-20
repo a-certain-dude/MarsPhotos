1e4e17ecde7742c7d6df186f6b185c6a687226d0
Jose <nkwantabisajoseph25@gmail.com>
2025-05-20 20:09:39 +0000

    coil library to retrieve image from a web source

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")
     
     testImplementation("junit:junit:4.13.2")
     
