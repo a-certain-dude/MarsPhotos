2e9024300ddff579ae1bdeaa85469bf14615a633
Jose <nkwantabisajoseph25@gmail.com>
2025-05-22 19:58:09 +0000

    remove content scale

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -73,7 +73,7 @@ fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
         error = painterResource(R.drawable.ic_broken_image),
         
         placeholder = painterResource(R.drawable.loading_img ),
-        contentScale = ContentScale.Crop,
+      //  contentScale = ContentScale.Crop,
         modifier = modifier.fillMaxWidth()
     )
     
